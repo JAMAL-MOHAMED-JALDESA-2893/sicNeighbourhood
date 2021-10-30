6046bd6975923a02a2d443722abdbf74f28e5bab
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-10-30 22:00:39 +0300

    add database configs to .env file

--- sicneighborhood/settings.py
+++ sicneighborhood/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hl)m3l&b$8o#ed&z(%@2q@$mdd+7$a&q+ti@36fme#d37m5%2a'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
