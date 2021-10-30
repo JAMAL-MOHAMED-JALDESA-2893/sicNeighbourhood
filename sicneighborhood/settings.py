a90b85e3eaae6bf09f55551d1a399f946ba9ec29
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-10-30 22:13:46 +0300

    add imports to settings

--- sicneighborhood/settings.py
+++ sicneighborhood/settings.py
@@ -10,7 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+
+import os
 from pathlib import Path
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+import django_heroku
+import dj_database_url
+from decouple import config,Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
