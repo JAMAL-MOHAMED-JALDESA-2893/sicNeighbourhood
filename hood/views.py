b731d0924fdd5794c41adf9620604317bc62b972
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-10-31 18:47:05 +0300

    adds view biz and search view function

--- hood/views.py
+++ hood/views.py
@@ -115,3 +115,21 @@ def new_business(request):
     else:
         form = NewBusinessForm()
     return render(request, 'new_business.html', {"form": form})
+
+
+@login_required(login_url='/accounts/login')
+def view_biz(request, id):
+    biz = Business.objects.get(id=id)
+    return render(request, {'business':biz,
+    })
+
+@login_required(login_url='/accounts/login/')
+def search(request):
+    if 'business' in request.GET and request.GET['business']:
+        business = request.GET.get("business")
+        results = Business.search_business(business)
+        message = f'business'
+        return render(request, 'search.html', {'business': results, 'message': message})
+    else:
+        message = "You haven't searched for anything, please try again"
+    return render(request, 'search.html', {'message': message})
