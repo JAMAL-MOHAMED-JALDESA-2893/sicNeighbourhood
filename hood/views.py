292753f4623d2aba2a724cb092e3e757d20b6d76
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-10-31 18:39:18 +0300

    adds signup view function

--- hood/views.py
+++ hood/views.py
@@ -15,3 +15,20 @@ def index(request):
     # return HttpResponse('Hi there')
     hoods = Neighbourhood.objects.all()
     return render(request, 'index.html', {"hoods": hoods})
+
+
+def signup(request):
+    print('here')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+
+            login(request, user)
+            return redirect('login')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/registration_form.html', {'form': form})
