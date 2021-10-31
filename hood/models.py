2f90ea66a3aadbeae3636c97e2faa8245c8918b6
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-10-31 17:41:17 +0300

    add model class neighbourhood

--- hood/models.py
+++ hood/models.py
@@ -5,3 +5,10 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 # Create your models here.
+class Neighbourhood(models.Model):
+    hood_name = models.CharField(max_length=200)
+    hood_location = models.CharField(max_length=200)
+    hood_description = models.TextField(max_length=500, blank=True)
+    hood_photo = CloudinaryField('photo', default='photo')
+    admin = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='admin')
